d098447f1095e2987c161b119eede63d697bb2f9
phenolophthaleinum <mccv99@gmail.com>
2023-09-08 18:29:19 +0200

    - simple parsing of the tables into a pickle - pickle with AliasedDict - a dict with aliases to key - created AliasedDict: now alias can refer to multiple keys

--- table_parser.py
+++ table_parser.py
@@ -1,5 +1,33 @@
 import pandas as pd
+import dill
+from utils import AliasedDict
 
 
-df = pd.read_excel("./table_s4.xlsx", index_col=0, header=None, skiprows=[0])
-print(df)
+df = pd.read_excel("./table_s4.xlsx", index_col=0, skiprows=[0])
+alias_df = pd.read_excel("./table_s9.xlsx", skiprows=[0])
+
+# mirna gene ID (HUGO) are actually unique
+assert len(alias_df.iloc[:,0].unique()) == len(alias_df.iloc[:, 0])
+
+# base dict from all HUGO mirna id 
+db = df.to_dict(orient='index')
+
+# initialise aliased dict
+adb = AliasedDict(db)
+
+# assign aliases
+for key in adb:
+    mask = alias_df['miRNA gene ID (HUGO)'].values == key
+    row = alias_df.loc[mask]
+    try:
+        items ,= row.iloc[:, 1:4].values
+        for item in items:
+            if pd.isna(item):
+                continue
+            adb.add_alias(item, key)
+    except:
+        continue
+
+# save to dill
+with open("mirna_table.pkl", 'wb') as f:
+    dill.dump(adb, f)
--- utils.py
+++ utils.py
@@ -5,17 +5,44 @@ class AliasedDict(dict):
         self.aliases = {}
 
     def __getitem__(self, key):
-        return super().__getitem__(self.aliases.get(key, key))
+        item = self.aliases.get(key, key)
+        # print(item)
+        # return [super(AliasedDict, self).__getitem__(itemkey) for itemkey in itemlist]
+        try:
+            # itemkey ,= itemlist
+            return super().__getitem__(item)
+        except:
+            return [super(AliasedDict, self).__getitem__(itemkey) for itemkey in item]
+
+        # try:
+        #     return super().__getitem__(self.aliases.get(key, key))
+        # except(TypeError):
+        #     itemlist = self.aliases.get(key, key)
+        #     return [super().__getitem__(self.aliases.get(key, key)) for key in itemlist]
+
+        # print(item)
+        # if isinstance(itemlist, list):
+        #     return [super().__getitem__(self.aliases.get(key, key)) for key in itemlist]
+        # return super().__getitem__(self.aliases.get(key, key))
     
     def __setitem__(self, key, value):
         return super().__setitem__(self.aliases.get(key, key), value)
         
-    def add_alias(self, key_or_dict, alias=None):
-        if isinstance(key_or_dict, dict):
-            self.aliases.update(key_or_dict)
+    def add_alias(self, alias, key):
+        # key: list(alias)
+        # if alias in self.aliases:
+        #     self.aliases[alias].append(key_or_dict)
+        # else:
+        #     self.aliases[alias] = [key_or_dict]
+        # first, simplest - alias: key
+        # alias: list(key)
+        if alias in self.aliases:
+            # print(key_or_dict)
+            self.aliases[alias].append(key)
         else:
-            self.aliases.update({key_or_dict: alias})
+            self.aliases[alias] = [key]
     
     def show_aliases(self, key):
         return [alias for alias, value_of_key in self.aliases.items() if value_of_key == key]
-    
+
+
