41aa263aa22522ff5df12c124baad5cdf9659c08
phenolophthaleinum <mccv99@gmail.com>
2023-11-01 19:35:53 +0100

    - code clearing - added docstrings for data parsing from xlsx files - added animation for containers resizing - added version variable in page.py

--- utils.py
+++ utils.py
@@ -5,15 +5,7 @@ class AliasedDict(dict):
         self.aliases = {}
 
     def add_alias(self, alias, key):
-        # key: list(alias)
-        # if alias in self.aliases:
-        #     self.aliases[alias].append(key_or_dict)
-        # else:
-        #     self.aliases[alias] = [key_or_dict]
-        # first, simplest - alias: key
-        # alias: list(key)
         if alias in self.aliases:
-            # print(key_or_dict)
             self.aliases[alias].append(key)
         else:
             self.aliases[alias] = [key]
@@ -23,38 +15,18 @@ class AliasedDict(dict):
     
     def __getstate__(self):
         return {"aliases": self.aliases, "data": dict(self)}
-        # dict_inst = super().__getstate__()
-        # dict_inst['aliases'] = self.aliases
-        # return dict_inst
 
     def __setstate__(self, state):
         self.aliases = state["aliases"]
-        # self.clear()
-        # super().update(state["data"])
         self.update(state["data"])
-        # super().__setstate__(state)
-        # self.aliases = state['aliases']
 
     def __getitem__(self, key):
         item = self.aliases.get(key, key)
-        # print(item)
-        # return [super(AliasedDict, self).__getitem__(itemkey) for itemkey in itemlist]
         try:
-            # itemkey ,= itemlist
             return super().__getitem__(item)
         except:
             return [super(AliasedDict, self).__getitem__(itemkey) for itemkey in item]
 
-        # try:
-        #     return super().__getitem__(self.aliases.get(key, key))
-        # except(TypeError):
-        #     itemlist = self.aliases.get(key, key)
-        #     return [super().__getitem__(self.aliases.get(key, key)) for key in itemlist]
-
-        # print(item)
-        # if isinstance(itemlist, list):
-        #     return [super().__getitem__(self.aliases.get(key, key)) for key in itemlist]
-        # return super().__getitem__(self.aliases.get(key, key))
     def __setitem__(self, key, value):
         if not hasattr(self, 'aliases'):
             self.aliases = {}
